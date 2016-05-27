2a868b518614739a57341f3b9fa559709149358b
Piyush Maurya <piyush.maurya@live.in>
2016-05-27 12:01:23 +0530

    Update settings.py

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-
+DEVELOPERS = 'Piyush Maurya'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
